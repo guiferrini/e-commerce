2325e040bc824aa79b2225fc97ab6ef510ba8ea0
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-17 15:25:49 -0300

    Add ESLint

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 const PORT = 3333;
 
 app.get('/principal', (request, response) => {
-  response.json({message: 'ola mundo'});
+  response.json({ message: 'ola mundo' });
 });
 
 app.listen(PORT, () => {
-  console.log(`✅ Server Connected at Port 3️⃣ 3️⃣ 3️⃣ 3️⃣`)
+  console.log('✅ Server Connected at Port 3️⃣ 3️⃣ 3️⃣ 3️⃣');
 });
