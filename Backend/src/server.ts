9db26cae8b4ac680d29a36319cebe4ab84a10e7c
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-23 19:34:02 -0300

    Add CORS

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 const app = express();
 const PORT = 3333;
+
+app.use(cors());
+
 app.use(express.json());
 
 app.use(routes);
