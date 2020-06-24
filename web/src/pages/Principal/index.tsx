3a1d80f8c4071fcddbbfdc81c3c740cf14bdfcbe
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-24 17:03:01 -0300

    Add Principal: header static - index ans styles

--- web/src/pages/Principal/index.tsx
+++ web/src/pages/Principal/index.tsx
@@ -1,22 +1,77 @@
 import React from 'react';
+import { FiFacebook, FiInstagram, FiLinkedin, FiYoutube, FiAlertCircle } from 'react-icons/fi'
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
+
+import logo from '../../assets/Logo_Bodega_Ferrini.png';
 
 const Principal: React.FC = () => {
 
 return (
   <Container>
-    <header>
-      <h1>header</h1>
-    </header>
+    <Content>
+      <header>
+        <hgroup>
+          <a href='https://www.instagram.com/guiferrini/?hl=pt-br' target='_blank'>
+            <span><FiInstagram /></span>
+            <strong>Instagram</strong>
+          </a>
+          <a href='https://www.linkedin.com/in/guilherme-ferrini-44678256/' target='_blank'>
+            <span><FiLinkedin /></span>
+            <strong>Linkedin</strong>
+          </a>
+          <a href='https://github.com/guiferrini' target='_blank'>
+            <span><FiFacebook /></span>
+            <strong>Facebook</strong>
+          </a>
+          <a href='https://www.youtube.com/' target='_blank'>
+            <span><FiYoutube /></span>
+            <strong>Youtube</strong>
+          </a>
+        </hgroup>
+        <figure>
+          <img src={logo} alt="Bodega_Ferrini_Wine_House" />
+          <h1>Bodega Ferrini - Wine House</h1>
+        </figure>
+        <div>
 
-    <main>
-      <h1>corpo</h1>
-    </main>
+          <input 
+            type="text"
+            name="email"
+            id="email"
+            placeholder="EMAIL"
+          />
+          <input 
+            type="text"
+            name="password"
+            id="password"
+            placeholder="PASSWORD"
+          />
+          <button type="submit">
+            Entrar
+          </button>
+          <Link to="/">
+            <FiAlertCircle/>
+            <h2>Esqueci minha senha</h2>
+          </Link>
+          <Link to="/">
+            <FiAlertCircle/>
+            <h2>Cadastro</h2>
+          </Link>
 
-    <footer>
-      <h1>rodape</h1>
-    </footer>
+
+        </div>
+      </header>
+
+      <main>
+        <h1>corpo</h1>
+      </main>
+
+      <footer>
+        <h1>rodape</h1>
+      </footer>
+    </Content>
   </Container>
 )
 }
--- web/src/pages/Principal/styles.ts
+++ web/src/pages/Principal/styles.ts
@@ -1,5 +1,102 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* margin: 0 20px 15px 20px; */
+  width: 100vw;
   
+  header{
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    /* background: #8c313d; */
+    background: #F0F0F5;
+    color: #8c313d;
+    height: auto;
+    padding: 5px;
+
+    hgroup {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      text-decoration: none;
+      padding: 15px;
+      align-items: stretch
+    }
+    
+    a {
+      text-decoration: none;
+      color: #8c313d;
+      display: flex;     
+    }
+
+    span {
+      margin-right: 5px;
+    }
+    
+    img {
+      padding: 10px;
+      width: 30%;
+    }
+
+    div {
+      display: flex;
+      flex-direction: column;
+      margin-right: 5px;
+      padding: 15px;
+
+      input {
+        margin-bottom: 6px;
+        padding: 5px;
+        color: black;
+        border-radius: 8px;
+        text-align: center;
+        height: 30px;
+        width: auto;
+      }
+
+      button {
+        margin-bottom: 6px;
+        border-radius: 8px;
+        padding: 5px;
+        height: 30px;
+        width: auto;
+        transition: background-color 0.2s;
+        
+        &:hover {
+          background: #8c313d;
+          color: #F0F0F5;
+          font-weight: bold;
+        }
+      }
+
+
+      a {
+        display: flex;
+        justify-content: flex-start;
+        transition: background-color 0.4s;
+        font-size: 15px;
+        margin-top: 10px;
+
+        &:hover {
+          color: #30121e;
+          font-weight: bold;
+          /* font-size: 22px; */
+        }
+
+        h2 {
+          margin-left: 5px;
+          font-size: 15px;
+        }
+      }
+    }
+
+  }
 `;
