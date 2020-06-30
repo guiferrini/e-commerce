5086f7fca915dae332a22f9f7a80c9acbbfe215f
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-06-30 17:56:56 -0300

    Add CreateUser: Header and Footer

--- web/src/pages/CreateUser/index.tsx
+++ web/src/pages/CreateUser/index.tsx
@@ -1,11 +1,94 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { 
+  FiFacebook, FiInstagram, FiLinkedin, FiYoutube, 
+  FiLogIn  
+} from 'react-icons/fi'
+import { Link, useHistory } from 'react-router-dom';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core'; // tipagem c tds funções do useRef.current
+import * as Yup from 'yup';
+
+import validationError from '../../erros/validationErros';
+import { AuthContext } from '../../context/AuthContext';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content } from './styles';
+
+import logo from '../../assets/Logo_Bodega_Ferrini.png';
+import argentina from '../../assets/wine_of_Argentina.jpg';
+import prices from '../../assets/special_prices.jpg';
+import week from '../../assets/wine_of_week.jpg';
 
 const CreateUser: React.FC = () => {
   return (
     <Container>
-      Create User
+      <Content>
+      <header>
+          <hgroup>
+            <a href='https://www.instagram.com/guiferrini/?hl=pt-br' target='_blank'>
+              <span><FiInstagram /></span>
+              <strong>Instagram</strong>
+            </a>
+            <a href='https://www.linkedin.com/in/guilherme-ferrini-44678256/' target='_blank'>
+              <span><FiLinkedin /></span>
+              <strong>Linkedin</strong>
+            </a>
+            <a href='https://github.com/guiferrini' target='_blank'>
+              <span><FiFacebook /></span>
+              <strong>Facebook</strong>
+            </a>
+            <a href='https://www.youtube.com/' target='_blank'>
+              <span><FiYoutube /></span>
+              <strong>Youtube</strong>
+            </a>
+          </hgroup>
+          <figure>
+            <img src={logo} alt="Bodega_Ferrini_Wine_House" />
+            <h1>Bodega Ferrini - Wine House</h1>
+          </figure>
+          <Link to='/'>
+            <FiLogIn/>
+            <h2>Back to Login</h2>
+          </Link>
+        </header>
+
+        <main>
+          <h1>main</h1>
+        </main>
+
+        <footer>
+          <hgroup>
+            <a href='https://www.instagram.com/guiferrini/?hl=pt-br' target='_blank'>
+              <span><FiInstagram /></span>
+              <strong>Instagram</strong>
+            </a>
+            <a href='https://www.linkedin.com/in/guilherme-ferrini-44678256/' target='_blank'>
+              <span><FiLinkedin /></span>
+              <strong>Linkedin</strong>
+            </a>
+          </hgroup>
+          <div>
+            <span>
+              <h1>contact@bf.com</h1>
+              <h1>Bodega Ferrini - Wine House</h1>
+            </span>
+          </div>
+          <hgroup>
+            <a href='https://github.com/guiferrini' target='_blank'>
+              <span><FiFacebook /></span>
+              <strong>Facebook</strong>
+            </a>
+            <a href='https://www.youtube.com/' target='_blank'>
+              <span><FiYoutube /></span>
+              <strong>Youtube</strong>
+            </a>
+          </hgroup>
+        </footer>
+
+      </Content>
     </Container>
   );
 };
--- web/src/pages/CreateUser/styles.ts
+++ web/src/pages/CreateUser/styles.ts
@@ -1,5 +1,131 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  height: 100vh;
+  display: flex;
+  align-items: stretch;
 `;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* margin: 0 20px 15px 20px; */
+  width: 100vw;
+
+  main {
+    background: blue;
+  }
+
+  footer{
+    background: blue;
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    background: #8c313d;
+    color: #F0F0F5;
+    height: auto;
+    padding: 5px;
+    font-size: 15px;
+
+    hgroup {
+      display: flex;
+      flex-direction: column;
+      /* justify-content: space-between; */
+      text-decoration: none;
+      padding: 15px;
+      /* align-items: stretch */
+    }
+    
+    a {
+      text-decoration: none;
+      color: #F0F0F5;
+      display: flex;     
+    }
+
+    span {
+      margin-right: 5px;
+    }
+    
+    img {
+      padding: 10px;
+      width: 10%;
+    }
+    
+    div {
+      align-self: center;
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+
+    }
+    span h1 {
+      margin-bottom: 5px;
+    }
+  }
+
+  header{
+    display: flex;
+    text-align: center;
+    justify-content: space-between;
+    /* background: #8c313d; */
+    background: #F0F0F5;
+    color: #8c313d;
+    height: auto;
+    padding: 5px;
+
+    hgroup {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-evenly;
+      text-decoration: none;
+      padding: 15px;
+      align-items: stretch
+    }
+    
+    a {
+      text-decoration: none;
+      color: #8c313d;
+      display: flex;     
+    }
+
+    span {
+      margin-right: 5px;
+    }
+    
+    img {
+      padding: 10px;
+      width: 30%;
+    }
+
+    /* form {
+      display: flex;
+      flex-direction: column;
+      margin-right: 5px;
+      padding: 15px; */
+
+      > a {
+        display: flex;
+        align-self: center;
+        align-items: center;
+        border-radius: 8px;
+        padding: 5px;
+        height: 30px;
+        width: 300px;
+        border: 2px solid black;
+        transition: background-color 0.2s;
+  
+        &:hover {
+          background: #8c313d;
+          color: #F0F0F5;
+          font-weight: bold;
+          border: 2px solid #8c313d;
+        } 
+        h2 {
+          margin-left: 10px;
+          font-size: 15px;
+        }
+      }
+    }
+  }
+`;
+
