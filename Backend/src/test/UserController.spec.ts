17e283b0db51dce8e96418a0d1331af1da4aa200
Guilherme Ferrini <guilhermemferrini@gmail.com>
2020-07-11 11:52:16 -0300

    Add Jest to test and config

---
+++ Backend/src/test/UserController.spec.ts
@@ -0,0 +1,16 @@
+// import { Request, Response } from 'express';
+// import { hash } from 'bcryptjs';
+// import UserController from '../controller/UserController';
+// import User from '../models/User';
+
+// // const usersRouter = Router();
+
+// const users: User[] = []; // ainda n tenho BD
+
+describe('User', () => {
+  it('should be able to create a User', () => {
+    // const users = 'gui';
+
+    expect(1 + 2).toBe(3);
+  });
+});
